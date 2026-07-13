a7c253746fbe199bc32180f184ac7bb7363bdf59
NagendraGandham <nagendragandham770@gmail.com>
2026-07-13 12:07:08 +0530

    Time: 3 ms (14.01%), Space: 43.1 MB (67.83%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,5 +1,6 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
+        
         String s = strs[0];
 
         for (String str : strs) {
@@ -11,12 +12,14 @@ class Solution {
         
         System.out.println(s);
         for (String str : strs) {
+            if (s.isEmpty()) {
+            return "";
+        }
             char[] ch=str.toCharArray();
             for (int i = 0; s.length() > i && i < ch.length ; i++) {
                     if (s.charAt(i) == (ch[i])) {
                         continue;
                     } else {
-                        
                         s = s.substring(0, i);
                         break;
                     }
