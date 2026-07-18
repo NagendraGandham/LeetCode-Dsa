ffebab4ab249c04506e65127966ce2f26137c217
NagendraGandham <nagendragandham770@gmail.com>
2026-07-18 16:14:13 +0530

    Time: 2 ms (36.79%), Space: 43.5 MB (26.64%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -3,32 +3,10 @@ class Solution {
         if(s.length()!=goal.length()){
             return false;
         }
-        char ch = s.charAt(0);
-        int index = -1;
-        for (int k = 0; k < goal.length(); k++) {
-            if (ch == goal.charAt(k)) {
-                index = k;
-                int size = s.length();
-                int i = index;
-                int j = 0;
-                do {
-                    if (s.charAt(j) == goal.charAt(i)) {
-                        i = (i + 1) % size;
-                        j = (j + 1) % size;
-                        System.out.println(i + " " + j);
-
-                    } 
-                    else{
-                       break; 
-                    }
-                } while (i != index);
-                if (index == i) {
-                    return true;
-                }
-
-            }
+        s=s+s;
+        if(s.contains(goal)){
+            return true;
         }
-
         return false;
     }
 }
