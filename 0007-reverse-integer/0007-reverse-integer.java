4ac3618992f2ca822b593e48c0792ae3ee4a0365
NagendraGandham <nagendragandham770@gmail.com>
2026-07-22 21:33:12 +0530

    Time: 1 ms (99.97%), Space: 42.5 MB (80.19%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,16 +1,14 @@
 class Solution {
     public int reverse(int x) {
-      
         long num=0;
         int copy=x;
         while(x!=0){
-            int n=x%10;
+            num=num*10+x%10;
             x=x/10;
-            num=num*10+n;
         }
-        if(num==(int)num)
+        if(num==(int)num){
             return (int)num;
+        }
         return 0;
-
     }
 }
