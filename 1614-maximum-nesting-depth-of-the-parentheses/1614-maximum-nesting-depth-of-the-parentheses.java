3252e6bccf5fdfd95728a9d6a398ef09bb1acc84
NagendraGandham <nagendragandham770@gmail.com>
2026-07-29 14:42:55 +0530

    Time: 1 ms (23.76%), Space: 42.7 MB (72.48%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maxDepth(String s) {
         int max=0;
-        Deque<Character> stack=new LinkedList<>();
+        Deque<Character> stack=new ArrayDeque<>();
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)=='('){
                 stack.addFirst('(');
