f845f845293b2b27d3f046c30f26fd65aa20fb41
NagendraGandham <nagendragandham770@gmail.com>
2026-07-29 22:12:46 +0530

    Time: 0 ms (100%), Space: 44.5 MB (69.81%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,41 +1,37 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int i=nums.length-1;
-        int j=nums.length-1;
-        int k=0;
-        while(i>0 && nums[i]<=nums[i-1]){
+        int i = nums.length - 1;
+        int j = nums.length - 1;
+        int k = 0;
+        while (i > 0 && nums[i] <= nums[i - 1]) {
             i--;
         }
-        if(i==0){
-            while(k<j){
-                int temp=nums[k];
-                nums[k]=nums[j];
-                nums[j]=temp;
+        if (i == 0) {
+            while (k < j) {
+                int temp = nums[k];
+                nums[k] = nums[j];
+                nums[j] = temp;
                 k++;
                 j--;
             }
             return;
         }
-        int min=i;
-        for( j=i+1;j<nums.length;j++){
-            System.out.println(j);
-            if(nums[j]>nums[i-1]){
-                min=j;
-            }
+         j = nums.length - 1;
+        while (nums[j] <= nums[i - 1]) {
+            j--;
         }
-       System.out.println(i-1+" "+min);
-        int temp=nums[i-1];
-        nums[i-1]=nums[min];       
-        nums[min]=temp;
 
-    k=nums.length-1;
-    while(i<k){
-        temp=nums[k];
-        nums[k]=nums[i];
-        nums[i]=temp;
-        i++;
-        k--;
-    }
-    
+        int min=j;
+        int temp = nums[i - 1];
+        nums[i - 1] = nums[min];
+        nums[min] = temp;
+        k = nums.length - 1;
+        while (i < k) {
+            temp = nums[k];
+            nums[k] = nums[i];
+            nums[i] = temp;
+            i++;
+            k--;
+        }
     }
 }
