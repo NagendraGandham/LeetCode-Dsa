db25cd98c23d4f55bf865ea33ece61cf27702892
NagendraGandham <nagendragandham770@gmail.com>
2026-08-02 11:28:46 +0530

    Time: 5 ms (32.82%), Space: 46.2 MB (94.05%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -3,12 +3,13 @@ class Solution {
         Map<Character,Integer> map=Map.of('I',1,'V',5,'X',10,'L',50,'C',100,'D',500,'M',1000);
         int num=0;
         for(int i=0;i<s.length();i++){
-            if(i<s.length()-1 && map.get(s.charAt(i))<map.get(s.charAt(i+1))){
-                 num-=map.get(s.charAt(i));
+            int curr= map.get(s.charAt(i));
+            if(i<s.length()-1 &&  curr<map.get(s.charAt(i+1))){
+                 num-=curr;
             
             }
             else
-            num+=map.get(s.charAt(i));
+            num+=curr;
         }
         return num;
 
