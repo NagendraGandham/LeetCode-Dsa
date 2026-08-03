98c041bf87a90b210e03eb7f4102cf826d18dc0b
NagendraGandham <nagendragandham770@gmail.com>
2026-08-03 11:25:00 +0530

    Time: 89 ms (5.1%), Space: 91.2 MB (77.09%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,26 +1,27 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        if(nums.length<1){
+        if (nums.length < 1) {
             return 0;
-    }
-        TreeMap<Integer,Integer> map=new TreeMap<>();
-        for(int i:nums){
-            map.put(i,1);
         }
-        int max=1;
-        int count=0;
-        int prev=map.firstKey()-1;
-        for(Map.Entry<Integer,Integer> m:map.entrySet())
-        {
-            if(prev+1==m.getKey()){
+        if(nums.length<2){
+            return 1;
+        }
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int i : nums) {
+            map.put(i, 1);
+        }
+        int max = 1;
+        int count = 0;
+        int prev = map.firstKey() - 1;
+        for (Map.Entry<Integer, Integer> m : map.entrySet()) {
+            if (prev + 1 == m.getKey()) {
                 count++;
+            } else {
+                max = Math.max(count, max);
+                count = 1;
             }
-            else{
-                max=Math.max(count,max);
-                count=1;
-            }
-            prev=m.getKey();
+            prev = m.getKey();
         }
-        return Math.max(max,count);
+        return Math.max(max, count);
     }
 }
