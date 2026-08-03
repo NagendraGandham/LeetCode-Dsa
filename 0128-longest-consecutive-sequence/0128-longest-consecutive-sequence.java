8b5eedcce329943d7446e92e4c888d7b0ec4e8f1
NagendraGandham <nagendragandham770@gmail.com>
2026-08-03 12:01:34 +0530

    Time: 29 ms (78.81%), Space: 95.6 MB (39.8%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -3,7 +3,7 @@ class Solution {
         if (nums.length < 1) {
             return 0;
         }
-        if(nums.length<2){
+        if (nums.length < 2) {
             return 1;
         }
         HashSet<Integer> set = new HashSet<>();
@@ -11,22 +11,21 @@ class Solution {
             set.add(i);
         }
         int max = 1;
-        int count = 1;
-        for (int i:set) {
-            
-            if(!set.contains(i-1)){
-                int key=i+1;
-            while(true){
-                if(set.contains(key)){
-                    count++;
-                    key++;
+
+        for (int i : set) {
+
+            if (!set.contains(i - 1)) {
+                int count = 1;
+                int key = i + 1;
+                while (true) {
+                    if (set.contains(key)) {
+                        count++;
+                        key++;
+                    } else {
+                        max = Math.max(count, max);
+                        break;
+                    }
                 }
-                else{
-                    max=Math.max(count,max);
-                    count=1;
-                    break;
-                }
-            }
             }
         }
         return max;
