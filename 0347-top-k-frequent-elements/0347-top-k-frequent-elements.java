43e6805abd125a654c2c87c1820b0264b975fcc6
NagendraGandham <nagendragandham770@gmail.com>
2026-09-25 11:03:40 +0530

    Time: 17 ms (20.01%), Space: 50.7 MB (6.22%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -0,0 +1,26 @@
+class Solution {
+    class frequency{
+        int num;
+        int freq;
+        frequency(int num,int freq){
+            this.num=num;
+            this.freq=freq;
+        }
+    }
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i:nums){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+       PriorityQueue<frequency> queue=new PriorityQueue<>((s1,s2)-> s2.freq-s1.freq);
+        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+            queue.offer(new frequency(entry.getKey(),entry.getValue()));
+        }
+        int[] arr=new int[k];
+        System.out.println(map);
+        while(k--!=0){
+            arr[arr.length-k-1]=queue.poll().num;
+        }
+        return arr;
+    }
+}
